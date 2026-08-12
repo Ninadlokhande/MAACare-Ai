05de22e5c28f66c561da93199be0935d5f820242
priyankagadekar325-blip <priyankagadekar325@gmail.com>
2026-08-12 14:20:17 +0530

    sop removed

--- src/main/java/com/sigma/Main.java
+++ src/main/java/com/sigma/Main.java
@@ -10,8 +10,6 @@ public class Main {
         System.out.println("Starting Application");
         System.out.println("Welcome to Maacare Ai");
         System.out.println("The Complete New World of Maternity HealthCare...");
-
-        System.out.println("Starting Application");
         Application.launch(Welcomepage.class, args);
 
     }
