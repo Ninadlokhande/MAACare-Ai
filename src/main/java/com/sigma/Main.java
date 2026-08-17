de43e0e5ad977e4118cc6516ee2c8ee632afa27c
rajdevsakshi2-hue <rajdevsakshi2@gmail.com>
2026-08-17 16:21:46 +0530

    conflict resolve main file

--- src/main/java/com/sigma/Main.java
+++ src/main/java/com/sigma/Main.java
@@ -2,12 +2,14 @@ package com.sigma;
 
 import com.sigma.view.DoctorBasicInformationPage;
 import com.sigma.view.DoctorDashboard;
+import com.sigma.view.Welcomepage;
+
 import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome");
-        Application.launch(DoctorDashboard.class, args);
+        Application.launch(Welcomepage.class, args);
     }
 }
